edddddacf9f3fcc3296a5f3aa36f7645179a9e87
Aasu703 <smellylips508@gmail.com>
2026-01-01 23:37:14 +0545

    For merge with main

--- lib/features/provider/domain/repository/provider_repository.dart
+++ lib/features/provider/domain/repository/provider_repository.dart
@@ -5,8 +5,11 @@ import 'package:petcare/features/provider/domain/entities/provider_entity.dart';
 // 100% Dart null safety
 
 abstract interface class IProviderRepository {
-  Future<Either<Failure, List<ProviderEntity>>> getAllProviders();
-  Future<Either<Failure, ProviderEntity>> getProviderById(String providerId);
+  Future<Either<Failure, List<ProviderEntity>>>
+  getAllProviders(); // parameterless
+  Future<Either<Failure, ProviderEntity>> getProviderById(
+    String providerId,
+  ); // parameterized
   Future<Either<Failure, bool>> createProvider(ProviderEntity entity);
   Future<Either<Failure, bool>> updateProvider(ProviderEntity entity);
   Future<Either<Failure, bool>> deleteProvider(String providerId);

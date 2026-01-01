210915e202ccdb6af425d9508bf77c4f395b0b58
Aasu703 <smellylips508@gmail.com>
2026-01-01 21:17:28 +0545

    completed LocalData for proivder

--- lib/features/provider/domain/repository/provider_repository.dart
+++ lib/features/provider/domain/repository/provider_repository.dart
@@ -2,7 +2,9 @@ import 'package:dartz/dartz.dart';
 import 'package:petcare/core/error/failures.dart';
 import 'package:petcare/features/provider/domain/entities/provider_entity.dart';
 
-abstract class ProviderRepository {
+// 100% Dart null safety
+
+abstract interface class IProviderRepository {
   Future<Either<Failure, List<ProviderEntity>>> getAllProviders();
   Future<Either<Failure, ProviderEntity>> getProviderById(String providerId);
   Future<Either<Failure, bool>> createProvider(ProviderEntity entity);

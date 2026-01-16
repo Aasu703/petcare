89680498c88697e3018eb5f08a181cc1d7254968
Aasu703 <smellylips508@gmail.com>
2026-01-16 13:02:59 +0545

    changes for main

--- lib/core/api/api_endpoints.dart
+++ lib/core/api/api_endpoints.dart
@@ -1,13 +1,13 @@
 class ApiEndpoints {
   ApiEndpoints._();
 
-  static const String baseUrl = 'http://localhost:5050/';
+  static const String baseUrl = 'http://10.0.2.2:5050/api/auth/';
 
   static const Duration connectionTimeout = Duration(seconds: 15);
   static const Duration receiveTimeout = Duration(seconds: 15);
 
   // -------------------------- AUTH -------------------------
-  static const String user = '/auth';
-  static const userLogin = '/auth/login';
-  static const userRegister = '/auth/register';
+  static const String user = 'user';
+  static const String userLogin = 'login';
+  static const String userRegister = 'register';
 }
